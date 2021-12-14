0476ccb668bee73bedcc2b57506a0c03482afe26
Meme <meme_es@hotmail.com>
2021-12-14 01:28:47 -0600

    Agrega codigo para Punto 1
    Este commit agrega el codigo del punto uno para pruebas dev. El programa
    recibe nombres de clientes hasta que el usuario indique que ya no desea
    ingresar ningun otro nombre, luego se imprimen los nombres que contengan
    los caracteres "RE".

---
+++ SortApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> clients = new List<string>();
+            string nombre = null;
+            char again;
+
+            do
+            {
+                Console.Write("Ingrese el nombre de un cliente: ");
+                nombre = Console.ReadLine();
+                clients.Add(nombre.ToUpper());
+
+                again = 'x';
+                while (again != 'S' && again != 's' && again != 'N' && again != 'n')
+                {
+                    Console.Write("Desea ingresar otro nombre (S/N)?: ");
+                    again = Console.ReadLine()[0];
+                }
+
+            } while (again == 'S' || again == 's');
+            Console.WriteLine("");
+
+            clients.ForEach(name =>
+            {
+                if (name.Contains("RE"))
+                {
+                    Console.WriteLine(name);
+                }
+            });
+
+            Console.Write("Ejecución finalizada, precione cualquier tecla");
+            Console.ReadKey();
+        }
+    }
+}
